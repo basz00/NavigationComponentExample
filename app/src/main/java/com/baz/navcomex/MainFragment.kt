98b172eb8cfc719a9b84c9144be54a3a381cc27e
Basofi Nugroho <basofi@attenza.nl>
2019-09-13 17:16:43 +0700

    Added first, second, and third fragment for main screen simulation

--- app/src/main/java/com/baz/navcomex/MainFragment.kt
+++ app/src/main/java/com/baz/navcomex/MainFragment.kt
@@ -7,26 +7,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_login.*
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.fragment_main.*
 
 /**
  * A simple [Fragment] subclass.
  */
-class LoginFragment : Fragment() {
+class MainFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        goToRegisterButton.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
-        }
-        goToMainButton.setOnClickListener {
-            findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
-        }
+        bottomNavigationView.setupWithNavController(findNavController())
     }
 }
